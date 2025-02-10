516ffc53f6a85f7b99490570d9ea03bc352d34e6
ehdwns778 <ehdwns778@naver.com>
2025-02-10 14:52:13 +0900

    몽고DB 연결

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
-// mongodb+srv://[]:BUFfDus9RCF9S3ff@mongo-study.jdr95.mongodb.net/
+// const mongodbUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}`;
+const mongodbUri = process.env.MONGO_URI as string;
 
-const mongodbUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}`;
-
-export const db = mongoose.createConnection(mongodbUri).asPromise();
+// export const db = mongoose.createConnection(mongodbUri).asPromise();
 
 (async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}`,{
-        dbName: 'hospital',
-      },
+      // `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}`,
+      mongodbUri,
+      {
+        dbName: "hospital",
+      }
     );
     console.log("Connected DB");
   } catch (error) {
